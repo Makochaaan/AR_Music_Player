d4fe8d26387624ca2b58916cb52da7c72c022009
Makochaaan <arowana326.315@gmail.com>
2024-12-11 14:20:06 +0900

    #2-[add]Make Interactive Functions

--- lib/page/unityPage.dart
+++ lib/page/unityPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_unity_widget/flutter_unity_widget.dart';
+import 'playerPage.dart';
 
 class UnityDemoScreen extends StatefulWidget {
 
@@ -12,8 +13,7 @@ class UnityDemoScreen extends StatefulWidget {
 }
 
 class _UnityDemoScreenState extends State<UnityDemoScreen> {
-  static final GlobalKey<ScaffoldState> _scaffoldKey =
-      GlobalKey<ScaffoldState>();
+  static final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   UnityWidgetController? _unityWidgetController;
 
   @override
@@ -31,6 +31,7 @@ class _UnityDemoScreenState extends State<UnityDemoScreen> {
             color: Colors.yellow,
             child: UnityWidget(
               onUnityCreated: onUnityCreated,
+              onUnityMessage: onUnityMessage,
             ),
           ),
         ),
@@ -41,5 +42,30 @@ class _UnityDemoScreenState extends State<UnityDemoScreen> {
   // Callback that connects the created controller to the unity controller
   void onUnityCreated(controller) {
     _unityWidgetController = controller;
+    // TODO:ゲームオブジェクトを10個作成。名称はimageIdに従う。それぞれのゲームオブジェクトに対して画像パスを渡す。
+    for (int i = 0; i < widget.pictureList.length; i++) {
+      String objectName = widget.pictureList[i]['ImageId'].toString();
+      _unityWidgetController!.postMessage(objectName,'methodName',widget.pictureList[i]['ImagePath']); // Send a message to the Unity game
+    }
+    // _unityWidgetController!.postMessage('gameObject','methodName','message'); // Send a message to the Unity game
   }
+
+  // Callback that receives messages from the Unity game
+  void onUnityMessage(message) {
+    print('Received message from Unity: ${message.toString()}');
+    int index = int.parse(message.toString());
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => playerPage(imageId: index),
+      ),
+    );
+  }
+
+}
+
+void main() {
+  runApp(const MaterialApp(
+    home: UnityDemoScreen(pictureList: []),
+  ));
 }
